bc3c7337b413e7ab44e36d7a8607a76f71c58e70
CodeFuller <CodeFuller@gmail.com>
2021-05-31 16:57:27 +0300

    Enable code analysis

--- src/IdentityServer/Startup.cs
+++ src/IdentityServer/Startup.cs
@@ -1,14 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace IdentityServer
 {
 	public class Startup
 	{
-		public void ConfigureServices(IServiceCollection services)
+		public void ConfigureServices()
 		{
 		}
 
